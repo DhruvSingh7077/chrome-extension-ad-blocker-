d62f4226f152a588babcd4b04766ddfb9bc02ae8
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-04-19 18:11:33 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,28 @@
+let countBtn =document.querySelector("#addBtn");
+let countEl =document.querySelector("#countEl");
+let count = 0;
+
+renderCount();
+countBtn.addEventListener("click",() =>{
+  count++;
+  countEl.textContent = count;
+ localStorage.setItem("count",count);
+});
+
+function renderCount(){
+  if(localStorage.getItem("count")){
+  count = localStorage.getItem("count");
+  countEl.textContent = count;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
